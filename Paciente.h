a2658c49ab7b57be98f41df38a89834f6f23b066
SEBASTIAN AFANADOR FONTAL <sebasaf@s2pc27.eisc.univalle.edu.co>
2016-12-02 08:00:34 -0500

    Clase paciente

---
+++ Paciente.h
@@ -0,0 +1,32 @@
+/*
+ * Autores: Jaime Cuartas 
+ * 			Emily Carvajal
+ * 			Sebastian Afanador
+ * Archivo: Paciente.h
+ * Descripcion: Definicion de la clase Paciente
+ * Fecha de Creacion: 02 de Diciembre del 2016
+ * Fecha de modificacion: 02 de Diciembre del 2016
+ * 
+ * */
+
+#ifndef PACIENTE_H
+#define PACIENTE_H
+
+#include <string>
+#include "historia.h"
+
+using std::string;
+
+class Paciente{
+	private:
+	int cedula;
+	string nombre;
+	string sexo;
+	int edad;
+	Historia *objHistoria;
+	
+	public:
+	Paciente(int cedula, string nombre, string sexo, int edad, string nombre2, string descripcion, string dia,string descripcion2);
+	~Paciente();
+	
+}
